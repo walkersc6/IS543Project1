2c6482f5c9bf0a3428e83a768c9f5225fc9ac1ff
Sarah Walker <walkersc6>
2025-10-09 08:56:23 -0600

    Initial Commit

---
+++ Walker Sarah Project 1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Walker Sarah Project 1
+//
+//  Created by Sarah Walker on 10/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Walker Sarah Project 1/Walker_Sarah_Project_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Walker_Sarah_Project_1App.swift
+//  Walker Sarah Project 1
+//
+//  Created by Sarah Walker on 10/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Walker_Sarah_Project_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
